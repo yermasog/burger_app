0214a2aa77e1f090e34d854056b1c4c0d5041dde
ymasog <yer.masog@gmail.com>
2021-02-09 21:17:01 -0600

    created db folder with connection.js and orm.js files

---
+++ config/orm.js
@@ -0,0 +1,32 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  selectAll: function(tableInput, colToSearch, valOfCol, cb) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      return cb (result);
+    });
+  },
+
+  insertOne: function(tableInput, colToSearch, valOfCol, cb) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      return cb (result);
+    });
+  },
+
+  updateOne: function(tableInput, colToSearch, valOfCol, cb) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      return cb (result);
+    });
+  }
+};
+
+module.exports = orm;
